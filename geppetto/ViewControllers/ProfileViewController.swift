bbdda997c2941223197a9ab813516cd5bdd7f8a5
Leonardo Rodrigues <leonardosrodrigues1@gmail.com>
2021-09-27 08:52:56 -0300

    Remove storage link from profile screen

--- geppetto/ViewControllers/ProfileViewController.swift
+++ geppetto/ViewControllers/ProfileViewController.swift
@@ -8,12 +8,6 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
-    @IBAction func storageButtonTapped() {
-        let storyboard = UIStoryboard(name: "Storage", bundle: nil)
-        let controller = storyboard.instantiateInitialViewController()
-        show(controller!, sender: self)
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
