642d945e3b2f00ba6a0d5c464642a0bcfeae0b82
Eduardo <42119808+eduardodini@users.noreply.github.com>
2021-10-01 09:54:21 -0300

    Esqueleto da tela de profile, com segmented control em funcionamento

--- geppetto/ViewControllers/ProfileViewController.swift
+++ geppetto/ViewControllers/ProfileViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
     @IBOutlet var profileImage: UIImageView!
     @IBOutlet var profileSegmentedControl: UISegmentedControl!
+    @IBOutlet var momentsContainer: UIView!
+    @IBOutlet var historyContainer: UIView!
+    @IBOutlet var favoritesContainer: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = "Fulano de tal"
     }
 
+    @IBAction func segmentedControlChanged(_ sender: Any) {
+        
+        if profileSegmentedControl.selectedSegmentIndex == 0 {
+            momentsContainer.isHidden = false
+            historyContainer.isHidden = true
+            favoritesContainer.isHidden = true
+        } else if profileSegmentedControl.selectedSegmentIndex == 1 {
+            momentsContainer.isHidden = true
+            favoritesContainer.isHidden = false
+            historyContainer.isHidden = true
+        } else {
+            momentsContainer.isHidden = true
+            favoritesContainer.isHidden = true
+            historyContainer.isHidden = false
+        }
+        
+    }
 }
