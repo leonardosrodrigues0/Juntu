e72fdee4e612d53819058c6ab64dde8d4d5c1286
Renato Máximo <renatonoronhamaximo@gmail.com>
2021-10-08 12:14:54 +0000

    fix: Controlador de páginas para os passos da atividade - índice, cor e Analytics

---
+++ geppetto/Views/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  geppetto
+//
+//  Created by Renato Noronha Máximo on 06/10/21.
+//
+
+import UIKit
+
+extension UIColor {
+    public class var accentColor: UIColor { UIColor(named: "AccentColor") ?? UIColor.white }
+    public class var pageTintColor: UIColor { UIColor(named: "pageTintColor") ?? UIColor.white }
+}
