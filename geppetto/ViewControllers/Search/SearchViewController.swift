e59adb58f9340217db787a01effd70b120677300
Leonardo Rodrigues <leonardosrodrigues1@gmail.com>
2021-10-18 19:51:09 -0300

    Add collection view to search screen  and cells, but fixed width for cards

--- geppetto/ViewControllers/Search/SearchViewController.swift
+++ geppetto/ViewControllers/Search/SearchViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 public class SearchViewController: UIViewController {
     
+    private let tagCellIdentifier = "cell"
+    private var items = [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+        "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+        "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+        "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+        "41", "42", "43", "44", "45", "46", "47", "48", "49", "50"
+    ]
+    
     // MARK: - Properties
     var resultsController: SearchResultsViewController
     var searchController: UISearchController
@@ -39,4 +48,24 @@ public class SearchViewController: UIViewController {
         navigationItem.searchController = searchController
         definesPresentationContext = true
     }
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: tagCellIdentifier, for: indexPath) as? TagCardCell
+        cell?.tagCard = TagCard()
+        return cell!
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegate {
+    
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected cell \(indexPath.item)!")
+    }
+    
 }
---
+++ geppetto/Views/Storyboards/Search/TagCardCell.swift
@@ -0,0 +1,14 @@
+//
+//  TagCardCell.swift
+//  geppetto
+//
+//  Created by Leonardo de Sousa Rodrigues on 18/10/21.
+//
+
+import UIKit
+
+class TagCardCell: UICollectionViewCell {
+    
+    @IBOutlet var tagCard: TagCard!
+    
+}
