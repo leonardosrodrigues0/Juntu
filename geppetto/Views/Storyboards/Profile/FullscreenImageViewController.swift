0cb6727207c879030f3ba216ecc5e1f033c6a55a
Gabriel <bimuelas@gmail.com>
2021-10-27 10:20:17 -0400

    Show actions sheet when "more" button is tapped

--- geppetto/Views/Storyboards/Profile/FullscreenImageViewController.swift
+++ geppetto/Views/Storyboards/Profile/FullscreenImageViewController.swift
@@ -46,13 +46,51 @@ class FullscreenImageViewController: UIViewController {
             target: self,
             action: #selector(didTapCloseButton(sender:))
         )
+        
         // define more button in navbar
         navItem.rightBarButtonItem = UIBarButtonItem(
             image: UIImage(systemName: "ellipsis"),
             style: .plain,
             target: self,
-            action: nil
+            action: #selector(didTapMoreButton(sender:))
         )
+    }
+    
+    private func createActionSheet() -> UIAlertController {
+        //create alert
+        let alert = UIAlertController(
+            title: nil,
+            message: nil,
+            preferredStyle: .actionSheet
+        )
+        
+        // create actions
+        let shareAction = UIAlertAction(title: "Compartilhar", style: .default) {_ in
+            print("Compartilhar")
+        }
+        
+        let useAsProfileImageAction = UIAlertAction(title: "Usar como foto do perfil", style: .default) {_ in
+            print("Usar como foto do perfil")
+        }
+        
+        let goToActivityAction = UIAlertAction(title: "Ir para atividade", style: .default) {_ in
+            print("Ir para atividade")
+        }
+        
+        let deletePhotoAction = UIAlertAction(title: "Apagar foto", style: .destructive) {_ in
+            print("Apagar foto")
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        // add actions to alert
+        alert.addAction(shareAction)
+        alert.addAction(useAsProfileImageAction)
+        alert.addAction(goToActivityAction)
+        alert.addAction(deletePhotoAction)
+        alert.addAction(cancelAction)
+        
+        return alert
     }
     
     // MARK: - Actions
@@ -60,5 +98,11 @@ class FullscreenImageViewController: UIViewController {
     /// Dismiss this ViewController. Triggered when close button is tapped
     @objc private func didTapCloseButton(sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    /// Show action sheet when more button is tapped
+    @objc private func didTapMoreButton(sender: UIBarButtonItem) {
+        let actionSheet = createActionSheet()
+        present(actionSheet, animated: true)
     }
 }
